0c47265092b781141696a6f704bd3ab5bf97c4c3
Ishank Dua <ishank@github.com>
2020-06-08 09:27:41 +0530

    Till Class in Java

---
+++ A4U2/src/class3/Student.java
@@ -0,0 +1,46 @@
+package class3;
+
+public class Student {
+
+	int s1id = 1;
+	String s1name = "Ishank";
+	
+	int s2id = 2;
+	String s2name = "Aditya";
+	
+	int s3id = 3;
+	String s3name = "Ravi";
+	
+	int s4id = 4;
+	String s4name = "Manish";
+	
+	int s5id = 5;
+	String s5name = "Akash";
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1 = new Student();
+		System.out.println(s1.s1id);
+		System.out.println(s1.s1name);
+		
+		Student s2 = new Student();
+		System.out.println(s2.s2id);
+		System.out.println(s2.s2name);
+		
+		Student s3 = new Student();
+		System.out.println(s3.s3id);
+		System.out.println(s3.s3name);
+		
+		Student s4 = new Student();
+		System.out.println(s4.s4id);
+		System.out.println(s4.s4name);
+		
+		Student s5 = new Student();
+		System.out.println(s5.s5id);
+		System.out.println(s5.s5name);
+
+	}
+
+}
